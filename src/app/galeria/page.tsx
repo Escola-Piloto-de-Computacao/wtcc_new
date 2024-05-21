6087ea713bb1b9c703cbef6abc075e337d48430e
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-05-21 10:15:34 -0300

    Galeria page beginning

---
+++ src/app/galeria/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Galeria() {
+    return (
+        <div className="h-[81vh] flex flex-col gap-3">
+            <h1 className="text-center text-4xl">Galeria</h1>
+
+        </div>
+    );
+};
