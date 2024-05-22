a8c2e9fc6a12adfe646d71d709a0f0b2526a1e36
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-05-22 10:34:55 -0300

    gallery page responsiveness

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -6,24 +6,10 @@ import { Pagination, Image } from 'antd';
 
 export default function Galeria() {
 
-    const getPicturesSize = () => {
-        //if (window.innerWidth < 768) {
-        //return [150, 100];
-        //}
-        //else if (window.innerWidth < 1100) {
-        //return [159, 100];
-        //}
-        //else {
-        return [350, 240];
-        //}
-    };
-    const picturesSize = getPicturesSize();
-
     const [key, setKey] = useState(0);
     const [currentPage, setCurrentPage] = useState(1);
     const imagesPerPage = 6;
 
-    // calculate the images for the current page
     const indexOfLastImage = currentPage * imagesPerPage;
     const indexOfFirstImage = indexOfLastImage - imagesPerPage;
     const currentImages = imagens.slice(indexOfFirstImage, indexOfLastImage);
@@ -33,36 +19,33 @@ export default function Galeria() {
         setKey(prevKey => prevKey + 1);
     };
 
-    // calculate the number of empty divs required
-    const emptyDivsCount = imagesPerPage - currentImages.length;
-
     return (
-        <div className="h-[97vh] flex flex-col items-center gap-3">
+        <div className="mb-20 flex flex-col items-center gap-3">
             <h1 className="text-center text-4xl mb-8">Galeria</h1>
-            <div className="mb-2">
+            <div className="flex flex-row flex-wrap gap-7 px-24 justify-center">
+                <Image.PreviewGroup>
+                    {currentImages.map((image) => (
+                        <div key={image.index} className="col-span-6 md:col-span-4">
+                            <Image
+                                width={350}
+                                height={250}
+                                key={image.index + key}
+                                src={image.src}
+                                alt="Imagem de eventos passados"
+                                className="rounded-lg image-fade-in"
+                            />
+                            <p className="text-center text-xs sm:text-sm">{image.descricao}</p>
+                        </div>
+                    ))}
+                </Image.PreviewGroup>
+            </div>
+            <div className="mt-2">
                 <Pagination
                     defaultCurrent={1}
                     total={imagens.length}
                     pageSize={imagesPerPage}
                     onChange={handlePageChange}
                 />
-            </div>
-            <div className="grid grid-cols-12 gap-8 sm:mx-36">
-                <Image.PreviewGroup>
-                    {currentImages.map((image) => (
-                        <div key={image.index} className="col-span-6 md:col-span-4">
-                            <Image
-                                width={picturesSize[0]}
-                                height={picturesSize[1]}
-                                key={image.index + key}
-                                src={image.src}
-                                alt="Imagem de eventos passados"
-                                className="rounded-lg image-fade-in"
-                            />
-                            <p className=" text-center text-xs sm:text-sm">{image.descricao}</p>
-                        </div>
-                    ))}
-                </Image.PreviewGroup>
             </div>
         </div>
     );
