dfed1ed82b3a97efb6f11ab67f8800ffae800fe9
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-05-27 23:08:59 -0300

    mesa-redonda page animations

--- src/app/eventos/mesa-redonda/page.tsx
+++ src/app/eventos/mesa-redonda/page.tsx
@@ -1,7 +1,20 @@
+'use client';
+
 import { BsFillQuestionSquareFill, BsChatLeftDotsFill } from "react-icons/bs";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function MesaRedondaPage() {
+    const variants = {
+        hidden: { opacity: 0, x: -40 },
+        visible: { opacity: 1, x: 0 },
+    };
+
+    const variants2 = {
+        hidden: { opacity: 0, x: 40 },
+        visible: { opacity: 1, x: 0 },
+    };
+
     return (
         <div className="flex flex-col items-center gap-6 mb-24 lg:mb-16">
             <div className="w-full flex flex-col items-center text-center mb-1">
@@ -20,26 +33,52 @@ export default function MesaRedondaPage() {
                         height={900}
                         className="hidden lg:block col-span-3 lg:w-full h-full lg:h-[260px] object-cover rounded-md"
                         quality={100}
-                        loading='eager'
+                        loading='lazy'
                     />
                     <div className="flex flex-col gap-4 justify-center">
-                        <div className="flex items-center gap-3">
+                        <motion.div
+                            className="flex items-center gap-3"
+                            initial="hidden"
+                            animate="visible"
+                            variants={variants}
+                            transition={{ ease: "easeOut", duration: 0.3 }}
+                        >
                             <BsFillQuestionSquareFill className="text-5xl text-primary" />
                             <h3 className="text-2xl font-light">O que é?</h3>
-                        </div>
-                        <p className="col-span-5 flex flex-col items-center justify-center text-justify p-1 lg:p-0">
+                        </motion.div>
+                        <motion.p
+                            className="col-span-5 flex flex-col items-center justify-center text-justify p-1 lg:p-0"
+                            initial="hidden"
+                            animate="visible"
+                            variants={variants}
+                            transition={{ ease: "easeOut", duration: 0.4 }}
+                        >
                             A mesa redonda tem como objetivo discutir o futuro do mercado de trabalho, trazendo perspectivas de alunos e professores, bem como especialistas em recursos humanos. Este evento é uma oportunidade imperdível para aqueles que desejam entender as tendências emergentes e os desafios que moldarão o ambiente profissional nos próximos anos. Os participantes terão a chance de ouvir insights valiosos de estudantes que estão ingressando no mercado de trabalho, bem como de professores que possuem uma visão ampla e atualizada das transformações no mundo profissional. Os debates abordarão questões cruciais como a digitalização, a automação, o trabalho remoto, a diversidade e inclusão, e o desenvolvimento de novas habilidades necessárias para o sucesso na era moderna.
-                        </p>
+                        </motion.p>
+
                     </div>
                 </div>
                 <div className="flex lg:mr-24 lg:ml-16 lg:p-7 gap-12 bg-slate-200 p-2 rounded-lg">
                     <div className="flex flex-col gap-2 lg:gap-4 justify-center items-end">
-                        <div className="flex items-center gap-3">
+                        <motion.div
+                            className="flex items-center gap-3"
+                            initial="hidden"
+                            animate="visible"
+                            variants={variants2}
+                            transition={{ ease: "easeOut", duration: 0.4 }}
+                        >
                             <h3 className="pb-1 text-2xl font-light">O que esperar?</h3>
                             <BsChatLeftDotsFill className="text-5xl text-primary" />
-                        </div>
-                        <p className="col-span-5 flex flex-col items-center justify-center text-justify p-1 lg:p-0">
-                            Além das apresentações e discussões, haverá sessões interativas onde os participantes poderão fazer perguntas, compartilhar experiências e colaborar em soluções inovadoras. Este é um momento único para ampliar seu conhecimento, construir uma rede de contatos e se preparar para as transformações que estão por vir no mundo do trabalho. Não perca esta oportunidade de se atualizar e se engajar em um diálogo produtivo sobre o futuro do mercado de trabalho. Junte-se a nós para explorar como podemos, juntos, moldar um futuro profissional mais dinâmico, inclusivo e sustentável.                    </p>
+                        </motion.div>
+                        <motion.p
+                            className="col-span-5 flex flex-col items-center justify-center text-justify p-1 lg:p-0"
+                            initial="hidden"
+                            animate="visible"
+                            variants={variants2}
+                            transition={{ ease: "easeOut", duration: 0.5 }}
+                        >
+                            Além das apresentações e discussões, haverá sessões interativas onde os participantes poderão fazer perguntas, compartilhar experiências e colaborar em soluções inovadoras. Este é um momento único para ampliar seu conhecimento, construir uma rede de contatos e se preparar para as transformações que estão por vir no mundo do trabalho. Não perca esta oportunidade de se atualizar e se engajar em um diálogo produtivo sobre o futuro do mercado de trabalho. Junte-se a nós para explorar como podemos, juntos, moldar um futuro profissional mais dinâmico, inclusivo e sustentável.
+                        </motion.p>
                     </div>
                     <Image
                         src="/mesa-redonda-2.png"
@@ -48,7 +87,7 @@ export default function MesaRedondaPage() {
                         height={300}
                         className="hidden lg:block col-span-3 h-[260px] object-cover rounded-md"
                         quality={100}
-                        loading='eager'
+                        loading='lazy'
                     />
                 </div>
             </div>
