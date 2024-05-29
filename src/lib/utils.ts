752cd73e7c24dcd4790f363a58cac29b696474e5
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-05-29 08:26:53 -0300

    hide api key

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,19 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 };
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDNZ6ZwWLU38eyjALMscrWJKQYMPEFTZng",
+    apiKey: process.env.APIKEY,
     authDomain: "ix-wtcc.firebaseapp.com",
     projectId: "ix-wtcc",
     storageBucket: "ix-wtcc.appspot.com",
-    messagingSenderId: "349785809306",
-    appId: "1:349785809306:web:fdca7a976cf22207d79a23"
+    messagingSenderId: process.env.SENDERID,
+    appId: process.env.APPID
 };
 
 const firebase = initializeApp(firebaseConfig);
