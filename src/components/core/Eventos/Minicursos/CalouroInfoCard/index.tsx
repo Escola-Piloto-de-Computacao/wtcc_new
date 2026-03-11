5725a8a2c82eeb6f50567b924cd7d79eab673464
polianaaraujo <polianaaraujo210@gmail.com>
2026-03-11 16:25:53 -0300

    alteração infocard

--- src/components/core/Eventos/Minicursos/CalouroInfoCard/index.tsx
+++ src/components/core/Eventos/Minicursos/CalouroInfoCard/index.tsx
@@ -28,16 +28,15 @@ const CalouroInfoCard: React.FC<CalouroInfoCardProps> = ({ className }) => {
                     <div className="flex flex-col gap-1">
                         <p>Devido a natureza do evento do evento do WTCC ser voltado aos calouros, a maioria dos minicursos tem viés introdutório, seguem algumas recomendações de minicursos aos calouros:</p>
                         <ul className="list-disc ml-10 flex flex-col gap-1">
-                            <li><span className="font-semibold">Dia 23</span>: <span className="italic">HTML, CSS e Javascript (Web Básico)</span>; <span className="italic">Figma</span>; <span className="italic">Kotlin</span></li>
-                            <li><span className="font-semibold">Dia 24</span>: <span className="italic">Avaliação de Design de Interfaces</span></li>
+                            <li><span className="font-semibold">Dia 18</span>: <span className="italic">Introdução ao desenvolvimento de jogos com a Godot 4</span>; <span className="italic">Introdução a QA</span>;</li>
+                            <li><span className="font-semibold">Dia 19</span>: <span className="italic">Introdução a testes automatizados com Cypress</span></li>
                         </ul>
                     </div>
                     <div className="flex flex-col gap-1">
                         <p>Agora seguem algumas recomendações de minicursos a depender da área de mercado em que deseja ingressar:</p>
                         <ul className="list-disc ml-10 flex flex-col gap-1">
-                            <li><span className="font-semibold">Desenvolvimento Web</span>: <span className="italic">HTML, CSS e Javascript</span> (para quem é completo iniciante na área); <span className="italic">Ruby on Rails</span> (para quem já conhece o básico da área)</li>
-                            <li><span className="font-semibold">Design de Interfaces</span>: <span className="italic">Figma</span> (para quem quer entender como se introduzir an área); <span className="italic">Avaliação de Design de Interfaces</span> (para quem quer entender o que faz uma boa interface)</li>
-                            <li><span className="font-semibold">Desenvolvimento de Jogos</span>: <span className="italic">Introdução a Godot 4</span>; <span className="italic">Introdução a Unity</span>; Ambas ferramentas muito utilizadas no mercado atual</li>
+                            <li><span className="font-semibold">Design de Interfaces</span>: <span className="italic">Introdução a Design System: Conceitos e Implementações</span> (para quem quer aprender a produzir designs); <span className="italic">Avaliação de Design de Interfaces</span> (para quem quer entender o que faz uma boa interface)</li>
+                            <li><span className="font-semibold">Desenvolvimento de Jogos</span>: <span className="italic">Introdução a Godot 4</span>; ferramenta muito utilizadas no mercado atual</li>
                         </ul>
                     </div>
                     <div className="mt-2 mb-3 mr-5 flex flex-col gap-3 bg-slate-300 rounded-md p-2">
